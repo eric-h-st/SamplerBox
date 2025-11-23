4ba14d4e793662cf0fb31167cdb28f09cfbb299d
eric-h-st <erez_harari@hotmail.com>
2025-11-23 03:10:52 -0500

    Added IGNORE_MIDI_DEVICES to avoid the "MidiInAlsa::alsaMidiHandler: unknown MIDI input error!" issue caused by unused MIDI ports, which also causes for lingering and/or missing notes

--- samplerbox.py
+++ samplerbox.py
@@ -441,7 +441,7 @@ midi_in = [rtmidi.MidiIn(b'in')]
 previous = []
 while True:
     for port in midi_in[0].ports:
-        if port not in previous and b'Midi Through' not in port:
+        if port not in IGNORE_MIDI_DEVICES and port not in previous and b'Midi Through' not in port:
             midi_in.append(rtmidi.MidiIn(b'in'))
             midi_in[-1].callback = MidiCallback
             midi_in[-1].open_port(port)
